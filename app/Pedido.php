be047c951acfb2538139820602f0c2a848f33840
Anthony Eduardo <105107191+AnthonyEduardo5@users.noreply.github.com>
2022-12-27 19:26:45 -0300

    belongsToMany de Pedido para Produtos
    Relacionamento NxN, onde é feito um relacionamento entre duas tabelas, sendo necessário a criação de uma terceira tabela.

--- app/Pedido.php
+++ app/Pedido.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    //
+    public function produtos() {
+        //return $this->belongsToMany('App\Produto', 'pedidos_produtos');
+        return $this->belongsToMany('App\Produto', 'pedidos_produtos', 'pedido_id', 'produto_id');
+        /*
+            1 - Representa a segunda tabela que faz parte do relacionamento NxN
+            2 - Representa a tabela auxiliar que recebe os dados do relacionamento
+            3 - Representa a FK do model(tabela) em que colocamos o codigo do relacionamento
+            4 - Representa a FK da segunda tabela que faz parte do relacionamento
+
+        */
+    }
 }
