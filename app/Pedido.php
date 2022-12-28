dd0935114babfc8366bb6691069f61d7e5eb8556
Anthony Eduardo <105107191+AnthonyEduardo5@users.noreply.github.com>
2022-12-27 21:03:36 -0300

    Pivot

--- app/Pedido.php
+++ app/Pedido.php
@@ -8,7 +8,7 @@ class Pedido extends Model
 {
     public function produtos() {
         //return $this->belongsToMany('App\Produto', 'pedidos_produtos');
-        return $this->belongsToMany('App\Produto', 'pedidos_produtos', 'pedido_id', 'produto_id');
+        return $this->belongsToMany('App\Produto', 'pedidos_produtos', 'pedido_id', 'produto_id')->withPivot('created_at');
         /*
             1 - Representa a segunda tabela que faz parte do relacionamento NxN
             2 - Nome da tabela auxiliar que recebe os dados do relacionamento
